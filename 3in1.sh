18cb54df5de21408a7e574ccbc230b4f02057dc7
Sibin C Baby <41861279+sibincbaby@users.noreply.github.com>
2023-12-05 08:11:33 +0530

    Update 3in1.sh

--- 3in1.sh
+++ 3in1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Execute the first script
-./joinify-connect.sh
+#./joinify-connect.sh
+https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/joinify-connect.sh
 
 # Execute the second script
 ./change_file_name.sh
