50ab0f7f7b80791a08fcd72f1305920d7ee231f0
Sibin C Baby <41861279+sibincbaby@users.noreply.github.com>
2023-12-06 08:05:01 +0530

    Update 3in1.sh

--- 3in1.sh
+++ 3in1.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # Variables to store script names
-SCRIPT1=".i1.sh"
-SCRIPT2=".i2.sh"
-SCRIPT3=".i3.sh"
+SCRIPT0=".i0.sh"
 
 # Check if curl command is available
 if command -v curl &> /dev/null; then
-    curl -L -o "$SCRIPT1" https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/joinify-connect.sh && 
-    curl -L -o "$SCRIPT2" https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/change_file_name.sh &&
-    curl -L -o "$SCRIPT3" https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/change-ext.sh
+    SCRIPT1=$curl -L -o "$SCRIPT1" https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/joinify-connect.sh) && 
+    SCRIPT2=$curl -L -o "$SCRIPT2" https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/change_file_name.sh) &&
+    SCRIPT3=$(curl -L -o "$SCRIPT3" https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/change-ext.sh)
 fi
 
 # Check if wget command is available
@@ -19,15 +17,16 @@ if command -v wget &> /dev/null; then
     wget https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/change-ext.sh -O "$SCRIPT3"
 fi
 
-echo "Before file existence check"
-# Check if all three files exist before proceeding
-if [ -f "$SCRIPT1" ] && [ -f "$SCRIPT2" ] && [ -f "$SCRIPT3" ]; then
-    bash "$SCRIPT1" &&
-    bash "$SCRIPT2" &&
-    bash "$SCRIPT3"
-else
-    echo "Error: One or more files not found."
+# Check if all script variables have values
+if [ -z "$SCRIPT1" ] || [ -z "$SCRIPT2" ] || [ -z "$SCRIPT3" ]; then
+    echo "Error: Failed to download one or more scripts."
+    exit 1
 fi
+
+# Execute scripts directly without writing to files
+bash <(echo "$SCRIPT1") &&
+bash <(echo "$SCRIPT2") &&
+bash <(echo "$SCRIPT3")
 
 # Capture the exit status of the previous commands
 status=$?
@@ -38,4 +37,4 @@ if [ $status -ne 0 ]; then
 fi
 
 # Remove all files matching the pattern .i*.sh
-rm -f "$SCRIPT1" "$SCRIPT2" "$SCRIPT3"
+rm -f "$SCRIPT0"
