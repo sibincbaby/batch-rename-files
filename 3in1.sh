be0543ac7d4dd33d7ff556ac7e82a3bf5a1ebf39
Sibin C Baby <41861279+sibincbaby@users.noreply.github.com>
2023-12-06 07:58:56 +0530

    Update 3in1.sh

--- 3in1.sh
+++ 3in1.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
+# Variables to store script names
+SCRIPT1=".i1.sh"
+SCRIPT2=".i2.sh"
+SCRIPT3=".i3.sh"
+
 # Check if curl command is available
 if command -v curl &> /dev/null; then
-    curl -L -o .i1.sh https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/joinify-connect.sh && 
-    curl -L -o .i2.sh https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/change_file_name.sh &&
-    curl -L -o .i3.sh https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/change-ext.sh
+    curl -L -o "$SCRIPT1" https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/joinify-connect.sh && 
+    curl -L -o "$SCRIPT2" https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/change_file_name.sh &&
+    curl -L -o "$SCRIPT3" https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/change-ext.sh
 fi
 
 # Check if wget command is available
 if command -v wget &> /dev/null; then
-    wget https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/joinify-connect.sh -O .i1.sh &&
-    wget https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/change_file_name.sh -O .i2.sh &&
-    wget https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/change-ext.sh -O .i3.sh
+    wget https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/joinify-connect.sh -O "$SCRIPT1" &&
+    wget https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/change_file_name.sh -O "$SCRIPT2" &&
+    wget https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/change-ext.sh -O "$SCRIPT3"
 fi
 
 echo "Before file existence check"
 # Check if all three files exist before proceeding
-if [ -f .i1.sh ] && [ -f .i2.sh ] && [ -f .i3.sh ]; then
-    bash .i1.sh &&
-    bash .i2.sh &&
-    bash .i3.sh
+if [ -f "$SCRIPT1" ] && [ -f "$SCRIPT2" ] && [ -f "$SCRIPT3" ]; then
+    bash "$SCRIPT1" &&
+    bash "$SCRIPT2" &&
+    bash "$SCRIPT3"
 else
     echo "Error: One or more files not found."
 fi
@@ -33,4 +38,4 @@ if [ $status -ne 0 ]; then
 fi
 
 # Remove all files matching the pattern .i*.sh
-rm -f .i*.sh
+rm -f "$SCRIPT1" "$SCRIPT2" "$SCRIPT3"
