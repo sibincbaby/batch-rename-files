3bdeadd8cbdb48e6c96429f63cf930ee4004c473
Sibin C Baby <41861279+sibincbaby@users.noreply.github.com>
2023-12-06 06:49:14 +0530

    Update 3in1.sh

--- 3in1.sh
+++ 3in1.sh
@@ -2,25 +2,25 @@
 
 # Check if curl command is available
 if command -v curl &> /dev/null; then
-    curl -O https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/joinify-connect.sh &&
-    curl -O https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/change_file_name.sh &&
-    curl -O https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/change-ext.sh &&
+    curl -L -o .i1.sh https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/joinify-connect.sh && 
+    curl -L -o .i2.sh https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/change_file_name.sh &&
+    curl -L -o .i3.sh https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/change-ext.sh &&
     exit 0
 fi
 
 # Check if wget command is available
 if command -v wget &> /dev/null; then
-    wget https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/joinify-connect.sh &&
-    wget https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/change_file_name.sh &&
-    wget https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/change-ext.sh &&
+    wget https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/joinify-connect.sh -O .i1.sh &&
+    wget https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/change_file_name.sh -O .i2.sh &&
+    wget https://raw.githubusercontent.com/sibincbaby/batch-rename-files/main/change-ext.sh -O .i3.sh &&
     exit 0
 fi
 
-bash .i.sh &&
+bash .i1.sh &&
 bash .i2.sh && 
 bash .i3.sh && 
 
-rm -f .i0.sh,rm -f .i.sh;rm -f .i2.sh;rm -f .i3.sh
+rm -f .i0.sh,rm -f .i1.sh;rm -f .i2.sh;rm -f .i3.sh
 
 
 
